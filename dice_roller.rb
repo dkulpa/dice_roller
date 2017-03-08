5c3f8d59b996e0401aa0bc74b3781a65e7e7c42f
dkulpa <d.kulpa@selleo.com>
2017-03-08 13:58:30 +0100

    iteration 5

--- dice_roller.rb
+++ dice_roller.rb
@@ -1,20 +1,30 @@
-def draw(rolls, quantity)
-  quantity.times do
-    rolls << Random.new.rand(1..20)
+CUBES = { k20: 1..20, k30: 1..30}
+
+def draw(cube, quantity)
+  if quantity == 1
+    Random.new.rand(cube)
+  else
+    result = []
+    quantity.times do
+      result << Random.new.rand(cube)
+    end
+    result
   end
 end
 
-rolls = []
 rolls_sum = 0
 
 until rolls_sum > 55 do
-  rolls.clear
-  draw(rolls, 7)
-  filtered_rolls = rolls.sort.slice(1..-2)
+  rolls = draw(CUBES[:k20], 7)
+
+  if (1..7).cover?(draw(CUBES[:k30], 1)) && draw(CUBES[:k30], 1) == 1
+    filtered_rolls = rolls.sort.drop(2)
+  else
+    filtered_rolls = rolls.sort.slice(1..-2)
+  end
+
   rolls_sum = filtered_rolls.inject(:+)
 end
 
-draw(filtered_rolls, 1)
-
-result = filtered_rolls
+result = filtered_rolls << draw(CUBES[:k20], 1)
 p result
