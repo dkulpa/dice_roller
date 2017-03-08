8b63c26f844899ee418d81ae2d1b6286ac2fa720
dkulpa <d.kulpa@selleo.com>
2017-03-08 12:32:11 +0100

    iteration 3

--- dice_roller.rb
+++ dice_roller.rb
@@ -1,8 +1,18 @@
-rolls = []
-
-7.times do
-  rolls << Random.new.rand(1..20)
+def draw(rolls, quantity)
+  quantity.times do
+    rolls << Random.new.rand(1..20)
+  end
 end
 
-result = rolls.sort.slice(1..-2)
+rolls = []
+rolls_sum = 0
+
+until rolls_sum > 55 do
+  rolls.clear
+  draw(rolls, 7)
+  filtered_rolls = rolls.sort.slice(1..-2)
+  rolls_sum = filtered_rolls.inject(:+)
+end
+
+result = filtered_rolls
 p result
