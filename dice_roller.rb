8989ebac4db72d22ac45cbcc2b52f18ea8307e5f
dkulpa <d.kulpa@selleo.com>
2017-03-08 14:01:08 +0100

    iteration 6

--- dice_roller.rb
+++ dice_roller.rb
@@ -13,12 +13,15 @@ def draw(cube, quantity)
 end
 
 rolls_sum = 0
+additional_roll = draw(CUBES[:k20], 1)
 
 until rolls_sum > 55 do
   rolls = draw(CUBES[:k20], 7)
 
   if (1..7).cover?(draw(CUBES[:k30], 1)) && draw(CUBES[:k30], 1) == 1
     filtered_rolls = rolls.sort.drop(2)
+  elsif (24..30).cover?(draw(CUBES[:k30], 1))
+    additional_roll = 25
   else
     filtered_rolls = rolls.sort.slice(1..-2)
   end
@@ -26,5 +29,5 @@ until rolls_sum > 55 do
   rolls_sum = filtered_rolls.inject(:+)
 end
 
-result = filtered_rolls << draw(CUBES[:k20], 1)
+result = filtered_rolls << additional_roll
 p result
